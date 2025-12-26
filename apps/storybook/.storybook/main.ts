c17c3856aaaa5d7e65e0764bb5c5d7ed94103eb5
gw.park <gw.park@twolinecloud.com>
2025-12-26 18:41:35 +0900

    chore: update project configuration and remove unused files
    - Expanded .gitignore to include additional environment and build files.
    - Added lint-staged configuration to package.json for improved code quality checks.
    - Removed obsolete PostCSS and Tailwind configuration files, along with related styles.
    - Deleted unused assets and story files to streamline the project structure.
    - Updated TypeScript configuration for several hooks to enhance type safety and maintainability.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -8,9 +8,8 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-vite",
