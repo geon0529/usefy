82cafcb1601c6b3cbbd868917aee8925fa8fe21a
gw.park <gw.park@twolinecloud.com>
2026-01-04 13:40:03 +0900

    chore: switch Storybook theme from light to dark for improved aesthetics

--- apps/storybook/.storybook/manager.ts
+++ apps/storybook/.storybook/manager.ts
@@ -2,7 +2,7 @@ import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming/create";
 
 const theme = create({
-  base: "light",
+  base: "dark",
   brandTitle: "Usefy",
   brandUrl: "https://github.com/geon0529/usefy",
   brandTarget: "_blank",
